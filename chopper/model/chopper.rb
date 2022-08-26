27c0abcb16535a2185d41a1092b8984a43ba2f24
vickyylopezz <vlopez@fi.uba.ar>
2022-08-26 16:58:56 -0300

    Actualizo logica Chopper.sum

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -8,9 +8,20 @@ class Chopper
   end
 
   def sum(array)
-    return 'cuatro' if array.size > 1
-    return 'uno' if array.size == 1
+    return 'vacio' if array.size.zero?
 
-    'vacio'
+    suma = 0
+    array.each do |element|
+      suma += element
+    end
+
+    return to_words(suma / 10) + ',' + to_words(suma % 10) if suma > 9
+
+    to_words(suma)
+  end
+
+  def to_words(number)
+    numbers = %w[cero uno dos tres cuatro cinco seis siete ocho nueve]
+    numbers[number]
   end
 end
