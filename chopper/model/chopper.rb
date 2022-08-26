a62f5859de825c1d34a8798c3ebb13e27c492b82
vickyylopezz <vlopez@fi.uba.ar>
2022-08-26 16:03:47 -0300

    Solucion final Chopper.chop

--- chopper/model/chopper.rb
+++ chopper/model/chopper.rb
@@ -1,6 +1,8 @@
 class Chopper
-  def chop(position, array) # rubocop:disable Lint/UnusedMethodArgument
-    return 0 if array.size.positive?
+  def chop(position, array)
+    array.each_with_index do |element, index|
+      return index if element == position
+    end
 
     -1
   end
--- chopper/spec/chopper_spec.rb
+++ chopper/spec/chopper_spec.rb
@@ -10,10 +10,10 @@ describe 'Chopper' do
   it 'chop de 3 y [3] deberia ser 0' do
     expect(chopper.chop(3, [3])).to eq 0
   end
-=begin # rubocop:disable all
   it 'chop de 3 y [0,7,3] deberia ser 2' do
     expect(chopper.chop(3, [0,7,3])).to eq 2
   end
+=begin # rubocop:disable all
 
   it 'sum de [] deberia ser vacio' do
     expect(chopper.sum([])).to eq 'vacio'
