6baa5105130a9ed5cfd9a81fd124c97cb6697df2
Victoria Abril Lopez <vlopez@fi.uba.ar>
2022-08-27 13:06:54 +0000

    Agrego kilometros a calcular alquiler

--- alquiler/model/alquiler.rb
+++ alquiler/model/alquiler.rb
@@ -1,5 +1,6 @@
 class Alquiler
   def calcular(parametros_alquiler, tipo_alquiler) # rubocop:disable Lint/UnusedMethodArgument
+    return 130 if tipo_alquiler == 'k'
     return 10_000 if tipo_alquiler == 'd'
 
     300
--- alquiler/spec/alquiler_spec.rb
+++ alquiler/spec/alquiler_spec.rb
@@ -4,10 +4,14 @@ describe 'Alquiler' do
   let(:alquiler) { Alquiler.new }
 
   it 'alquiler de 3 horas deberia ser 300' do
-    expect(alquiler.calcular(3,"h")).to eq 300 # rubocop:disable Lint/AmbiguousOperator
+    expect(alquiler.calcular(3,"h")).to eq 300
   end
 
   it 'alquiler de 5 dias deberia ser 10000' do
-    expect(alquiler.calcular(5,"d")).to eq 10000 # rubocop:disable Lint/AmbiguousOperator
+    expect(alquiler.calcular(5,"d")).to eq 10000
+  end
+
+  it 'alquiler de 2 km deberia ser 130' do
+    expect(alquiler.calcular(2,"k")).to eq 130
   end
 end
