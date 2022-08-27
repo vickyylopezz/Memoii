74aeb1441a92ee48fabf720bbda026a113c56a85
vickyylopezz <vlopez@fi.uba.ar>
2022-08-27 12:08:07 -0300

    Agrego logica para app con entrada/salida

--- alquiler/app.rb
+++ alquiler/app.rb
@@ -1,3 +1,11 @@
 require_relative './model/alquiler'
 
-puts 200
+fecha_inicio = ARGV[0]
+fecha_fin = ARGV[1]
+cuit = ARGV[2]
+tipo = ARGV[3]
+cant_alquiler = ARGV[4]
+
+resultado = Alquiler.new.calcular(cant_alquiler.to_i, tipo, cuit, fecha_inicio.to_i, fecha_fin.to_i)
+
+puts "importe: #{resultado}"
--- alquiler/spec/app_spec.rb
+++ alquiler/spec/app_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe 'App' do
   it 'alquiler de 2 horas es 200' do
     resultado = `ruby app.rb 20190119 20190119 20112223336 h 2`
-    expect(resultado.strip).to eq "200"
+    expect(resultado.strip).to eq "importe: 200"
+  end
+
+  it 'alquiler de 1 dia es 2000' do
+    resultado = `ruby app.rb 20190119 20190119 20112223336 d 1`
+    expect(resultado.strip).to eq "importe: 2000"
   end
 end
+
