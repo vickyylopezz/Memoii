61aa157d4cbcb74453856ede700722bf179d0eb3
Victoria Abril Lopez <vlopez@fi.uba.ar>
2022-08-27 13:01:46 +0000

    Actualizo logica calcular de Alquiler

--- alquiler/model/alquiler.rb
+++ alquiler/model/alquiler.rb
@@ -1,5 +1,7 @@
 class Alquiler
   def calcular(parametros_alquiler, tipo_alquiler) # rubocop:disable Lint/UnusedMethodArgument
+    return 10_000 if tipo_alquiler == 'd'
+
     300
   end
 end
--- alquiler/spec/alquiler_spec.rb
+++ alquiler/spec/alquiler_spec.rb
@@ -6,4 +6,8 @@ describe 'Alquiler' do
   it 'alquiler de 3 horas deberia ser 300' do
     expect(alquiler.calcular(3,"h")).to eq 300 # rubocop:disable Lint/AmbiguousOperator
   end
+
+  it 'alquiler de 5 dias deberia ser 10000' do
+    expect(alquiler.calcular(5,"d")).to eq 10000 # rubocop:disable Lint/AmbiguousOperator
+  end
 end
