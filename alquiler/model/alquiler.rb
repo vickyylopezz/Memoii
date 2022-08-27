93125f0a6c10951b3722145b5eb4e562b1bad776
vickyylopezz <vlopez@fi.uba.ar>
2022-08-27 11:32:20 -0300

    Agrego penalizacion por devolver despues el auto

--- alquiler/model/alquiler.rb
+++ alquiler/model/alquiler.rb
@@ -1,5 +1,5 @@
 class Alquiler
-  def calcular(parametros_alquiler, tipo_alquiler, cuit)
+  def calcular(parametros_alquiler, tipo_alquiler, cuit, fecha_alquiler, fecha_devolucion)
     costo = 0
     costo = 100 * parametros_alquiler if tipo_alquiler == 'h'
 
@@ -7,11 +7,18 @@ class Alquiler
 
     costo = 100 + 10 * parametros_alquiler if tipo_alquiler == 'k'
 
-    aplicar_descuento(costo, cuit)
+    costo = aplicar_descuento(costo, cuit)
+    aplicar_recargo(costo, fecha_alquiler, fecha_devolucion)
   end
 
   def aplicar_descuento(monto, cuit)
     return monto - monto * 0.05 if cuit.match(/^26/)
+
+    monto
+  end
+
+  def aplicar_recargo(monto, fecha_alquiler, fecha_devolucion)
+    return 4000 if fecha_devolucion > fecha_alquiler
 
     monto
   end
