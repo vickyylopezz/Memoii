bc484d345ce1ef4e7c1d87f0747f40047a3c0eb6
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 18:14:58 -0300

    Agrego test para calcular puntaje en humano con espada

--- lucha/model/humano.rb
+++ lucha/model/humano.rb
@@ -1,5 +1,7 @@
 class Humano
-  def calcular_puntaje_con(_arma)
+  def calcular_puntaje_con(arma)
+    return 2 if arma.instance_of? Espada
+
     1
   end
 end
--- lucha/spec/humano_spec.rb
+++ lucha/spec/humano_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe 'Humano' do
   let(:humano) {FactoryPersonaje.new.crear_personaje("humano")}
   let(:mano) {FactoryArma.new.crear_arma("mano")}
+  let(:espada) {FactoryArma.new.crear_arma("espada")}
 
   it '01-Puntaje de humano-con-mano es 1' do
     expect(Humano.new.calcular_puntaje_con(mano)).to eq 1
+  end
+
+  it '02-Puntaje de humano-con-espada es 2' do
+    expect(Humano.new.calcular_puntaje_con(espada)).to eq 2
   end
 end
