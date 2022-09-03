5cee29b63e7981648c139c8bb25a431b9a7c0271
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 18:28:36 -0300

    Refactor, extraccion de variables en test de luchador

--- lucha/spec/luchador_spec.rb
+++ lucha/spec/luchador_spec.rb
@@ -4,21 +4,22 @@ describe 'Luchador' do
   let(:espada) {FactoryArma.new.crear_arma("espada")}
   let(:mano) {FactoryArma.new.crear_arma("mano")}
   let(:humano) {FactoryPersonaje.new.crear_personaje("humano")}
+  let(:lobo) {FactoryPersonaje.new.crear_personaje("lobo")}
 
   it '01-Puntaje de humano-con-espada en estadio es 2' do
-    expect(Luchador.new(Humano.new, FactoryArma.new.crear_arma("espada"), Estadio.new).puntaje).to eq 2
+    expect(Luchador.new(humano, espada, Estadio.new).puntaje).to eq 2
   end
 
   it '02-Puntaje de humano-con-mano en estadio es 1' do
-    expect(Luchador.new(Humano.new, FactoryArma.new.crear_arma("mano"), Estadio.new).puntaje).to eq 1
+    expect(Luchador.new(humano, mano, Estadio.new).puntaje).to eq 1
   end
 
   it '03-Puntaje de lobo-con-mano en estadio es 3' do
-    expect(Luchador.new(Lobo.new, FactoryArma.new.crear_arma("mano"), Estadio.new).puntaje).to eq 3
+    expect(Luchador.new(lobo, mano, Estadio.new).puntaje).to eq 3
   end
 
   it '04-Puntaje de lobo-con-espada en estadio es 6' do
-    expect(Luchador.new(Lobo.new, FactoryArma.new.crear_arma("espada"), Estadio.new).puntaje).to eq 6
+    expect(Luchador.new(lobo, espada, Estadio.new).puntaje).to eq 6
   end
 
   it '05-Puntaje de humano-con-espada en ciudad es 4' do
