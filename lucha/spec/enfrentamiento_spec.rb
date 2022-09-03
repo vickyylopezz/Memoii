0d01399be523188f116a3290b6a0cd9620f8c89f
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 15:51:13 -0300

    Agrego tests de enfrentamiento entre humano-lobo con mano y espada

--- lucha/spec/enfrentamiento_spec.rb
+++ lucha/spec/enfrentamiento_spec.rb
@@ -34,4 +34,12 @@ describe 'Enfrentamiento' do
   it '08- Lobo-con-espada vs Humano-con-espada en estadio da como resultado gana 1' do
     expect(enfrentamiento.luchar(Luchador.new(Lobo.new, FactoryArma.new.crear_arma("espada"), Estadio.new), Luchador.new(Humano.new, FactoryArma.new.crear_arma("espada"), Estadio.new))).to eq  'gana 1'
   end
+
+  it '09-Humano-con-espada vs Lobo-con-mano en estadio da como resultado gana 2' do
+    expect(enfrentamiento.luchar(Luchador.new(Humano.new, FactoryArma.new.crear_arma("espada"), Estadio.new), Luchador.new(Lobo.new, FactoryArma.new.crear_arma("mano"), Estadio.new))).to eq  'gana 2'
+  end
+
+  it '10-Humano-con-mano vs Lobo-con-espada en estadio da como resultado gana 2' do
+    expect(enfrentamiento.luchar(Luchador.new(Humano.new, FactoryArma.new.crear_arma("mano"), Estadio.new), Luchador.new(Lobo.new, FactoryArma.new.crear_arma("espada"), Estadio.new))).to eq  'gana 2'
+  end
 end
