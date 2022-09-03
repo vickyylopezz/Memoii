f08cd075e7e12d3d36041edfe40c7072d6d96ee8
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 18:32:33 -0300

    Agrego caso de empate en ciudad

--- lucha/model/enfrentamiento.rb
+++ lucha/model/enfrentamiento.rb
@@ -2,8 +2,6 @@ require_relative 'ciudad'
 
 class Enfrentamiento
   def luchar(luchador_uno, luchador_dos)
-    return 'gana 1' if luchador_uno.escenario.instance_of? Ciudad
-
     if luchador_uno.puntaje > luchador_dos.puntaje
       'gana 1'
     elsif luchador_uno.puntaje < luchador_dos.puntaje
