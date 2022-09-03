bb3b745f6b68b5412fc9547bfeb97958e237ebae
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 18:20:07 -0300

    Agrego test para calcular puntaje en lobo con espada

--- lucha/model/lobo.rb
+++ lucha/model/lobo.rb
@@ -1,5 +1,7 @@
 class Lobo
-  def calcular_puntaje_con(_arma)
+  def calcular_puntaje_con(arma)
+    return 6 if arma.instance_of? Espada
+
     3
   end
 end
--- lucha/spec/lobo_spec.rb
+++ lucha/spec/lobo_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe 'Humano' do
   let(:lobo) {FactoryPersonaje.new.crear_personaje("lobo")}
   let(:mano) {FactoryArma.new.crear_arma("mano")}
+  let(:espada) {FactoryArma.new.crear_arma("espada")}
 
   it '01-Puntaje de lobo-con-mano es 3' do
     expect(lobo.calcular_puntaje_con(mano)).to eq 3
+  end
+
+  it '02-Puntaje de lobo-con-espada es 6' do
+    expect(lobo.calcular_puntaje_con(espada)).to eq 6
   end
 end
