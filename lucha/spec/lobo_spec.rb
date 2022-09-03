127406a690e85003c9fdf026fe89c32ad73fb76f
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 19:44:36 -0300

    Agrego test para calcular puntaje de lobo en ciudad

--- lucha/spec/lobo_spec.rb
+++ lucha/spec/lobo_spec.rb
@@ -5,6 +5,7 @@ describe 'Humano' do
   let(:mano) {FactoryArma.new.crear_arma("mano")}
   let(:espada) {FactoryArma.new.crear_arma("espada")}
   let(:estadio) {FactoryEscenario.new.crear_escenario("estadio")}
+  let(:ciudad) {FactoryEscenario.new.crear_escenario("ciudad")}
 
   it '01-Puntaje de lobo-con-mano es 3' do
     expect(lobo.calcular_puntaje_con(mano)).to eq 3
@@ -14,7 +15,11 @@ describe 'Humano' do
     expect(lobo.calcular_puntaje_con(espada)).to eq 6
   end
 
-  it '03-Puntaje de lobo es estadio es 3' do
+  it '03-Puntaje de lobo en estadio es 3' do
     expect(lobo.calcular_puntaje_es(estadio)).to eq 3
+  end
+
+  it '04-Puntaje de lobo en ciudad es 3' do
+    expect(lobo.calcular_puntaje_es(ciudad)).to eq 3
   end
 end
