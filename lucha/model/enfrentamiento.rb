834ff0fc1fae5fb63a1eb7600607299f8db7ae75
vickyylopezz <vlopez@fi.uba.ar>
2022-09-03 16:51:13 -0300

    Agrego logica de puntaje en luchar

--- lucha/model/enfrentamiento.rb
+++ lucha/model/enfrentamiento.rb
@@ -3,13 +3,12 @@ require_relative 'lobo'
 
 class Enfrentamiento
   def luchar(luchador_uno, luchador_dos)
-    return 'gana 2' if luchador_dos.personaje.instance_of? Lobo
-    return 'gana 1' if luchador_uno.personaje.instance_of? Lobo
-
-    return 'empate' if luchador_uno.arma.class == luchador_dos.arma.class
-
-    return 'gana 1' if luchador_uno.arma.instance_of? Espada
-
-    'gana 2'
+    if luchador_uno.puntaje > luchador_dos.puntaje
+      'gana 1'
+    elsif luchador_uno.puntaje < luchador_dos.puntaje
+      'gana 2'
+    else
+      'empate'
+    end
   end
 end
