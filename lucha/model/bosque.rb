3afc4b95a3c78b0c63d3735d21391a36c11d2fb5
vickyylopezz <vlopez@fi.uba.ar>
2022-09-04 12:53:40 -0300

    Refactor calcular puntaje para escenario bosque

--- lucha/model/bosque.rb
+++ lucha/model/bosque.rb
@@ -1,2 +1,14 @@
 class Bosque
+  INCREMENTO_BOSQUE = 3
+  def incrementar_puntaje_para_humano(puntaje)
+    puntaje
+  end
+
+  def incrementar_puntaje_para_lobo(puntaje)
+    puntaje * INCREMENTO_BOSQUE
+  end
+
+  def incrementar_puntaje_para_vampiro(puntaje)
+    puntaje
+  end
 end
