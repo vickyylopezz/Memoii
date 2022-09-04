c8d9f1b00945062eaa02165aaec748b0320d1b87
vickyylopezz <vlopez@fi.uba.ar>
2022-09-04 10:01:35 -0300

    Refactor tests

--- lucha/spec/enfrentamiento_spec.rb
+++ lucha/spec/enfrentamiento_spec.rb
@@ -8,85 +8,87 @@ describe 'Enfrentamiento' do
   let(:lobo) {FactoryPersonaje.new.crear_personaje("lobo")}
   let(:vampiro) {FactoryPersonaje.new.crear_personaje("vampiro")}
   let(:noche) {FactoryEscenario.new.crear_escenario("noche")}
+  let(:estadio) {FactoryEscenario.new.crear_escenario("estadio")}
+  let(:ciudad) {FactoryEscenario.new.crear_escenario("ciudad")}
 
   it '01-Humano-con-espada vs Humano-con-espada en estadio da como resultado empate' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Estadio.new), Luchador.new(humano, espada, Estadio.new))).to eq  'empate'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, estadio), Luchador.new(humano, espada, estadio))).to eq  'empate'
   end
 
   it '02-Humano-con-espada vs Humano-con-mano en estadio da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Estadio.new), Luchador.new(humano, mano, Estadio.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, estadio), Luchador.new(humano, mano, estadio))).to eq  'gana 1'
   end
 
   it '03-Humano-con-mano vs Humano-con-mano en estadio da como resultado empate' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, mano, Estadio.new), Luchador.new(humano, mano, Estadio.new))).to eq  'empate'
+    expect(enfrentamiento.luchar(Luchador.new(humano, mano, estadio), Luchador.new(humano, mano, estadio))).to eq  'empate'
   end
 
   it '04-Humano-con-mano vs Humano-con-espada en estadio da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, mano, Estadio.new), Luchador.new(humano, espada, Estadio.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, mano, estadio), Luchador.new(humano, espada, estadio))).to eq  'gana 2'
   end
 
   it '05-Humano-con-mano vs Lobo-con-mano en estadio da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, mano, Estadio.new), Luchador.new(lobo, mano, Estadio.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, mano, estadio), Luchador.new(lobo, mano, estadio))).to eq  'gana 2'
   end
 
   it '06-Humano-con-espada vs Lobo-con-espada en estadio da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Estadio.new), Luchador.new(lobo, espada, Estadio.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, estadio), Luchador.new(lobo, espada, estadio))).to eq  'gana 2'
   end
 
   it '07-Lobo-con-mano vs Humano-con-mano en estadio da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, Estadio.new), Luchador.new(humano, mano, Estadio.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, estadio), Luchador.new(humano, mano, estadio))).to eq  'gana 1'
   end
 
   it '08- Lobo-con-espada vs Humano-con-espada en estadio da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, Estadio.new), Luchador.new(humano, espada, Estadio.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, estadio), Luchador.new(humano, espada, estadio))).to eq  'gana 1'
   end
 
   it '09-Humano-con-espada vs Lobo-con-mano en estadio da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Estadio.new), Luchador.new(lobo, FactoryArma.new.crear_arma("mano"), Estadio.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, estadio), Luchador.new(lobo, mano, estadio))).to eq  'gana 2'
   end
 
   it '10-Humano-con-mano vs Lobo-con-espada en estadio da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, FactoryArma.new.crear_arma("mano"), Estadio.new), Luchador.new(lobo, espada, Estadio.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, mano, estadio), Luchador.new(lobo, espada, estadio))).to eq  'gana 2'
   end
 
   it '11-Lobo-con-espada vs Humano-con-mano en estadio da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, Estadio.new), Luchador.new(humano, mano, Estadio.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, estadio), Luchador.new(humano, mano, estadio))).to eq  'gana 1'
   end
 
   it '12-Lobo-con-mano vs Humano-con-espada en estadio da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, Estadio.new), Luchador.new(humano, espada, Estadio.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, estadio), Luchador.new(humano, espada, estadio))).to eq  'gana 1'
   end
 
   it '13-Lobo-con-espada vs Lobo-con-mano en estadio da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, Estadio.new), Luchador.new(lobo, mano, Estadio.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, estadio), Luchador.new(lobo, mano, estadio))).to eq  'gana 1'
   end
 
   it '14-Lobo-con-mano vs Lobo-con-espada en estadio da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, Estadio.new), Luchador.new(lobo, espada, Estadio.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, estadio), Luchador.new(lobo, espada, estadio))).to eq  'gana 2'
   end
 
   it '15-Lobo-con-espada vs Lobo-con-espada en estadio da como resultado empate' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, Estadio.new), Luchador.new(lobo, espada, Estadio.new))).to eq  'empate'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, espada, estadio), Luchador.new(lobo, espada, estadio))).to eq  'empate'
   end
 
   it '16-Lobo-con-mano vs Lobo-con-mano en estadio da como resultado empate' do
-    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, Estadio.new), Luchador.new(lobo, mano, Estadio.new))).to eq  'empate'
+    expect(enfrentamiento.luchar(Luchador.new(lobo, mano, estadio), Luchador.new(lobo, mano, estadio))).to eq  'empate'
   end
 
   it '17-Humano-con-espada vs Lobo-con-mano en ciudad da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Ciudad.new), Luchador.new(lobo, mano, Ciudad.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, ciudad), Luchador.new(lobo, mano, ciudad))).to eq  'gana 2'
   end
 
   it '18-Humano-con-espada vs Humano-con-espada en ciudad da como resultado empate' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Ciudad.new), Luchador.new(humano, espada, Ciudad.new))).to eq  'empate'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, ciudad), Luchador.new(humano, espada, ciudad))).to eq  'empate'
   end
 
   it '19-Humano-con-mano vs Lobo-con-espada en ciudad da como resultado gana 2' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, mano, Ciudad.new), Luchador.new(lobo, espada, Ciudad.new))).to eq  'gana 2'
+    expect(enfrentamiento.luchar(Luchador.new(humano, mano, ciudad), Luchador.new(lobo, espada, ciudad))).to eq  'gana 2'
   end
 
   it '20-Humano-con-espada vs Humano-con-mano en ciudad da como resultado gana 1' do
-    expect(enfrentamiento.luchar(Luchador.new(humano, espada, Ciudad.new), Luchador.new(humano, mano, Ciudad.new))).to eq  'gana 1'
+    expect(enfrentamiento.luchar(Luchador.new(humano, espada, ciudad), Luchador.new(humano, mano, ciudad))).to eq  'gana 1'
   end
 
   it '21-Vampiro-con-espada vs Lobo-con-mano en noche da como resultado gana 1' do
