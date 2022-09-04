50efb7755b68764285bed178146e35de36db9ed3
vickyylopezz <vlopez@fi.uba.ar>
2022-09-04 12:50:26 -0300

    Refactor calcular puntaje para escenario lluvia

--- lucha/model/lluvia.rb
+++ lucha/model/lluvia.rb
@@ -1,2 +1,14 @@
 class Lluvia
+  DISMINUCION_LLUVIA = 1
+  def incrementar_puntaje_para_humano(puntaje)
+    puntaje
+  end
+
+  def incrementar_puntaje_para_lobo(puntaje)
+    puntaje
+  end
+
+  def incrementar_puntaje_para_vampiro(puntaje)
+    puntaje - DISMINUCION_LLUVIA
+  end
 end
