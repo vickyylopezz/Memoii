fca6145cf56902bdd499dd3f2a944e1173e13e39
vickyylopezz <vlopez@fi.uba.ar>
2022-09-04 12:32:25 -0300

    Refactor calcular puntaje para escenario estadio

--- lucha/model/estadio.rb
+++ lucha/model/estadio.rb
@@ -1,2 +1,13 @@
 class Estadio
+  def incrementar_puntaje_para_humano(puntaje)
+    puntaje
+  end
+
+  def incrementar_puntaje_para_lobo(puntaje)
+    puntaje
+  end
+
+  def incrementar_puntaje_para_vampiro(puntaje)
+    puntaje
+  end
 end
