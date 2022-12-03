41300d6b98707b4f81bf1785db3d6ac48e0d44ad
vickyylopezz <vlopez@fi.uba.ar>
2022-12-03 12:04:33 -0300

    feat: agrego auto con cilindrada 1600

--- cotizador_de_autos/app.rb
+++ cotizador_de_autos/app.rb
@@ -1,1 +1,9 @@
-puts 'ci:1'
+datos = ARGV[0]
+datos = datos.split('/')
+cilindrada = datos[1]
+
+if Integer(cilindrada) == 1000
+  puts 'ci:1'
+else
+  puts 'ci:1.6'
+end
--- cotizador_de_autos/spec/app_spec.rb
+++ cotizador_de_autos/spec/app_spec.rb
@@ -6,5 +6,9 @@ describe 'App' do
     expect(resultado.strip).to eq "ci:1"
   end
 
+  it 'coeficiente imporitivo de auto con cilindrada 1600 es 1,6' do
+    resultado = `ruby app.rb auto/1600`
+    expect(resultado.strip).to eq "ci:1.6"
+  end
 end
 
