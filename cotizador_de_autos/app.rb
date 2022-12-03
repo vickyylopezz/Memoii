163924b7fe6db49b241db4aec03dbcc91ee7b5b3
vickyylopezz <vlopez@fi.uba.ar>
2022-12-03 18:40:26 -0300

    refactor: agrego cotizador a app.rb

--- cotizador_de_autos/app.rb
+++ cotizador_de_autos/app.rb
@@ -1,20 +1,13 @@
 require_relative 'model/factory_tipo_vehiculo'
+require_relative 'model/cotizador'
 
 datos = ARGV[0]
 datos = datos.split('/')
 tipo = datos[0]
 cilindrada = datos[1]
 
-if tipo == 'camioneta' && Integer(cilindrada) == 2000
-  puts 'ci:3'
-elsif tipo == 'camioneta' && Integer(cilindrada) == 1000
-  puts 'ci:1'
-elsif tipo == 'camioneta'
-  puts 'ci:2'
-elsif Integer(cilindrada) == 1000
-  puts 'ci:1'
-elsif Integer(cilindrada) == 1600
-  puts 'ci:1'
-else
-  puts 'ci:2'
-end
+cotizador = Cotizador.new
+vehiculo = FactoryTipoVehiculo.new.crear_vehiculo(tipo)
+ci = cotizador.calcular_coeficiente_impositivo(vehiculo, Integer(cilindrada))
+
+puts "ci:#{ci}"
