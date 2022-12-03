98379a5cf6e1f7086b0f1627b5f9c4b38992fe13
vickyylopezz <vlopez@fi.uba.ar>
2022-12-03 12:07:36 -0300

    feat: agrego auto con cilindrada 2000

--- cotizador_de_autos/app.rb
+++ cotizador_de_autos/app.rb
@@ -4,6 +4,8 @@ cilindrada = datos[1]
 
 if Integer(cilindrada) == 1000
   puts 'ci:1'
-else
+elsif Integer(cilindrada) == 1600
   puts 'ci:1.6'
+else
+  puts 'ci:2'
 end
--- cotizador_de_autos/spec/app_spec.rb
+++ cotizador_de_autos/spec/app_spec.rb
@@ -6,9 +6,14 @@ describe 'App' do
     expect(resultado.strip).to eq "ci:1"
   end
 
-  it 'coeficiente imporitivo de auto con cilindrada 1600 es 1,6' do
+  it 'coeficiente imporitivo de auto con cilindrada 1600 es 1.6' do
     resultado = `ruby app.rb auto/1600`
     expect(resultado.strip).to eq "ci:1.6"
+  end
+
+  it 'coeficiente imporitivo de auto con cilindrada 2000 es 2' do
+    resultado = `ruby app.rb auto/2000`
+    expect(resultado.strip).to eq "ci:2"
   end
 end
 
