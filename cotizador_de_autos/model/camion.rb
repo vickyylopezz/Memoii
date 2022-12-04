0aeaa8c587e36a14315b76dfa0f3b6390fd1fc2e
vickyylopezz <vlopez@fi.uba.ar>
2022-12-04 11:20:02 -0300

    feat: agrego valor de mercado para camion con cilindrada 1000

--- cotizador_de_autos/model/camion.rb
+++ cotizador_de_autos/model/camion.rb
@@ -3,4 +3,8 @@ class Camion
   def coeficiente_impositivo(cilindrada)
     (BASE * cilindrada) / 1_000_000
   end
+
+  def valor_mercado(_kilometros, _cilindrada)
+    1000.0
+  end
 end
--- cotizador_de_autos/spec/camion_spec.rb
+++ cotizador_de_autos/spec/camion_spec.rb
@@ -15,4 +15,9 @@ describe Camion do
     camion = Camion.new
     expect(camion.coeficiente_impositivo(2000)).to eq 4
   end
+
+  it 'valor de mercado de camion con cilindrada 1000 y 1000 kilometros es 1000.0' do
+    camion = Camion.new
+    expect(camion.valor_mercado(1000, 1000)).to eq 1000.0
+  end
 end
